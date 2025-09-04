70743289acfff75d6cb31857e7674f52a5bb2754
Anthony <anthony.k@modefin.com>
2025-09-04 17:07:06 +0300

    second comit

---
+++ script.js
@@ -0,0 +1,20 @@
+const toggleBtn = document.getElementById("darkModeToggle");
+const body = document.body;
+
+// Load saved mode
+if (localStorage.getItem("theme") === "dark") {
+  body.classList.add("dark-mode");
+  toggleBtn.textContent = "☀️";
+}
+
+toggleBtn.addEventListener("click", () => {
+  body.classList.toggle("dark-mode");
+
+  if (body.classList.contains("dark-mode")) {
+    toggleBtn.textContent = "☀️";
+    localStorage.setItem("theme", "dark");
+  } else {
+    toggleBtn.textContent = "🌙";
+    localStorage.setItem("theme", "light");
+  }
+});
